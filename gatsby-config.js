4d47a81f3fd749e6b55804ac10a84b4effbc819d
Param-Harrison <paramanantham@live.com>
2019-03-01 16:58:39 +0200

    feat: update manifest to make PWA works

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   siteMetadata: {
     title: 'tamilcode.org',
-    description: 'அறிவே அழியா செல்வம்',
+    description: 'கல்வியே அழியா செல்வம்',
     siteUrl: 'https://tamilcode.org', // full path to blog - no ending slash
   },
   mapping: {
@@ -98,6 +98,54 @@ module.exports = {
           },
         ],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'tamilcode.org - கல்வியே அழியா செல்வம்',
+        short_name: 'tamilcode',
+        start_url: '/index.html',
+        background_color: 'white',
+        theme_color: '#666',
+        display: 'standalone',
+        icons: [
+          {
+            src: "/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    },
   ],
 };
